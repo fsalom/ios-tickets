710b375a7700bf1db66b70eff55ca643fe065a49
Fernando Salom <fer@rudo.es>
2024-11-15 18:24:34 +0100

    Add initial project

---
+++ ticketsTests/ticketsTests.swift
@@ -0,0 +1,16 @@
+//
+//  ticketsTests.swift
+//  ticketsTests
+//
+//  Created by Fernando Salom Carratala on 15/11/24.
+//
+
+import Testing
+
+struct ticketsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
