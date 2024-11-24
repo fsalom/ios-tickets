8122c50b6105b2c9d02bc5a9b36a889090ee8712
Fernando Salom <fer@rudo.es>
2024-11-24 17:19:52 +0100

    Add domain entities

--- tickets/Domain/Entities/Product.swift
+++ tickets/Domain/Entities/Product.swift
@@ -1,7 +1,24 @@
-struct Product {
+import Foundation
+
+struct Product: Identifiable {
+    var id = UUID().uuidString
     var name: String
     var quantity: Int
     var price_per_unit: Float
     var price: Float
     var weight: String?
+
+    var priceWithFormat: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "€"
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        formatter.locale = Locale(identifier: "es_ES")
+        if let formattedNumber = formatter.string(from: NSNumber(value: price)) {
+            return formattedNumber
+        } else {
+            return "Error al formatear el número"
+        }
+    }
 }
--- tickets/Domain/Entities/Ticket.swift
+++ tickets/Domain/Entities/Ticket.swift
@@ -9,4 +9,35 @@ struct Ticket: Identifiable {
     var date: String
     var email: String
     var location: String?
+
+    var totalWithFormat: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "€"
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        formatter.locale = Locale(identifier: "es_ES")
+        if let formattedNumber = formatter.string(from: NSNumber(value: total)) {
+            return formattedNumber
+        } else {
+            return "Error al formatear el número"
+        }
+    }
+
+    var dateWithFormat: String {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "dd/MM/yyyy HH:mm"
+        inputFormatter.locale = Locale(identifier: "es_ES")
+
+        if let date = inputFormatter.date(from: date) {
+            let outputFormatter = DateFormatter()
+            outputFormatter.dateFormat = "d 'de' MMMM 'de' yyyy"
+            outputFormatter.locale = Locale(identifier: "es_ES")
+
+            let formattedDate = outputFormatter.string(from: date)
+            return formattedDate
+        } else {
+            return "Error: No se pudo convertir la fecha."
+        }
+    }
 }
