fe818dffed2b40083928f4c32505c05cf651be54
Fernando Salom <fer@rudo.es>
2024-11-25 13:22:18 +0100

    Add memory datasource

--- tickets/Data/Repositories/TicketsRepository.swift
+++ tickets/Data/Repositories/TicketsRepository.swift
@@ -1,13 +1,22 @@
 class TicketsRepository: TicketsRepositoryProtocol {
     // MARK: - Properties
     private let remote: TicketsRemoteDataSourceProtocol
+    private let memory: TicketsMemoryDataSource
 
     // MARK: - Init
-    init(remote: TicketsRemoteDataSourceProtocol) {
+    init(remote: TicketsRemoteDataSourceProtocol,
+         memory: TicketsMemoryDataSource) {
         self.remote = remote
+        self.memory = memory
     }
 
     func getAll() async throws -> [Ticket] {
-        try await remote.getAll()
+        do {
+            return try memory.get()
+        } catch {
+            let tickets = try await remote.getAll()
+            memory.save(value: tickets)
+            return tickets
+        }
     }
 }
--- tickets/Data/Repositories/UserRepository.swift
+++ tickets/Data/Repositories/UserRepository.swift
@@ -1,17 +1,34 @@
 class UserRepository: UserRepositoryProtocol {
     // MARK: - Properties
     private let remote: UserRemoteDataSourceProtocol
+    private let userMemory: UserMemoryDataSource
+    private let fcmMemory: UserFCMMemoryDataSource
 
     // MARK: - Init
-    init(remote: UserRemoteDataSourceProtocol) {
+    init(remote: UserRemoteDataSourceProtocol,
+         userMemory: UserMemoryDataSource,
+         fcmMemory: UserFCMMemoryDataSource) {
         self.remote = remote
+        self.userMemory = userMemory
+        self.fcmMemory = fcmMemory
     }
 
     func get() async throws -> User {
-        try await remote.get()
+        do {
+            return try userMemory.get()
+        } catch {
+            let user = try await remote.get()
+            userMemory.save(value: user)
+            return user
+        }
     }
 
     func update(fcmToken: String) async throws {
-        try await remote.update(fcmToken: fcmToken)
+        do {
+            _ = try fcmMemory.get()
+        } catch {
+            _ = try await remote.update(fcmToken: fcmToken)
+            fcmMemory.save(value: fcmToken)
+        }
     }
 }
