c2bb43fe328e2ae119c96e20d85e7c9fcc64ed8d
Fernando Salom <fer@rudo.es>
2024-11-25 13:22:03 +0100

    Add new home view

--- tickets/Presentation/Screen/Home/HomeView.swift
+++ tickets/Presentation/Screen/Home/HomeView.swift
@@ -1,45 +1,75 @@
-//
-//  HomeView.swift
-//  tickets
-//
-//  Created by Fernando Salom Carratala on 16/11/24.
-//
-
 import SwiftUI
 
 struct HomeView: View {
     @StateObject var viewModel: HomeViewModel
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Button("Cerrar sesión") {
-            Task {
-                try? await Config.shared.authenticator.logout()
-            }
-        }
-        List(viewModel.uiState.tickets, id: \.id) { ticket in
-            NavigationLink {
-                DetailTicketBuilder().build(with: ticket)
-            } label: {
-                HStack {
-                    Image(.mercadonaLogo)
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .foregroundColor(Color.gray)
-                        .clipShape(Circle())
+        NavigationView {
+            ScrollView {
+                HStack{
                     VStack(alignment: .leading) {
-                        Text("Mercadona")
-                        Text("\(ticket.dateWithFormat)").font(.footnote)
+                        Text("324€")
+                            .font(.title)
+                            .bold()
+                        Text("Gastado este mes")
+                            .font(.footnote)
                     }
-
                     Spacer()
-                    Text("\(ticket.totalWithFormat)").bold()
-                }                
+                }
+                .padding(16)
+                LazyVStack(alignment: .leading) {
+                    ForEach(viewModel.uiState.tickets) { ticket in
+                        NavigationLink {
+                            DetailTicketBuilder().build(with: ticket)
+                        } label: {
+                            HStack {
+                                Image(.mercadonaLogo)
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(Color.gray)
+                                    .clipShape(Circle())
+                                VStack(alignment: .leading) {
+                                    Text("Mercadona")
+                                        .font(.body)
+                                    Text("\(ticket.dateWithFormat)")
+                                        .font(.footnote)
+                                }
+                                Spacer()
+                                Text("\(ticket.totalWithFormat)")
+                                    .font(.body)
+                                    .bold()
+                                Image(systemName: "chevron.right")
+                                    .foregroundStyle(Color.gray)
+                            }
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
+            }
+        }
+        .scrollIndicators(.hidden)
+        .navigationTitle("Mi cesta")
+        .navigationBarTitleDisplayMode(.automatic)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    print("Settings tapped")
+                }) {
+                    Image(systemName: "gearshape")
+                }
             }
 
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    print("Profile tapped")
+                }) {
+                    Image(systemName: "person.crop.circle")
+                }
+            }
         }
-        .listStyle(.plain)
-        .onAppear() {
+        .onAppear {
             viewModel.getAll()
             viewModel.checkAndRequestNotificationPermissionIfNecessary()
         }
