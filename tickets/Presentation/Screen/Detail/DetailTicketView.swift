27162e006889f7138d5e0f381ca9f8e845a7cf16
Fernando Salom <fer@rudo.es>
2024-11-30 22:18:43 +0100

    Update detail design

--- tickets/Presentation/Screen/Detail/DetailTicketView.swift
+++ tickets/Presentation/Screen/Detail/DetailTicketView.swift
@@ -4,31 +4,50 @@ struct DetailTicketView: View {
     @StateObject var viewModel: DetailTicketViewModel
 
     var body: some View {
-
-        HStack {
-            Image(.mercadonaLogo)
-                .resizable()
-                .frame(width: 40, height: 40)
-                .foregroundColor(Color.gray)
-                .clipShape(Circle())
-            VStack{
-                Text(viewModel.uiState.ticket.location ?? "").font(.footnote)
-                Text(viewModel.uiState.ticket.dateWithFormat).font(.footnote)
-            }
-            Spacer()
-        }
-        Text(viewModel.uiState.ticket.totalWithFormat)
-
         ScrollView {
-            ForEach(viewModel.uiState.ticket.products) { product in
+            LazyVStack(alignment: .leading, spacing: 16) {
                 HStack {
-                    Text("\(product.quantity) x \(product.name.lowercased())")
+                    Image(.mercadonaLogo)
+                        .resizable()
+                        .frame(width: 64, height: 64)
+                        .foregroundColor(Color.gray)
+                        .clipShape(Circle())
+                    VStack{
+                        Text(viewModel.uiState.ticket.location ?? "").font(.footnote)
+                    }
                     Spacer()
-                    Text("\(product.priceWithFormat)").bold()
                 }
-                .padding(4)
                 Divider()
+                    .padding(.horizontal, 64)
+                    .padding(.vertical, 8)
+                HStack{
+                    Text(viewModel.uiState.ticket.dateWithFormat).font(.footnote)
+                    Spacer()
+                    Text(viewModel.uiState.ticket.totalWithFormat)
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+                .padding(8)
+                Divider()
+                    .padding(.horizontal, 64)
+                    .padding(.vertical, 8)
+                ForEach(viewModel.uiState.ticket.products) { product in
+                    HStack {
+                        Text("\(product.quantity) x \(product.name.lowercased())")
+                        Spacer()
+                        Text("\(product.priceWithFormat)").bold()
+                    }
+                    .padding(4)
+                    //Divider()
+                }
             }
+            .padding(16)
+            .background(
+                RoundedRectangle(cornerRadius: 16.0)
+                    .fill(Color.white)
+                    .shadow(color: Color.gray.opacity(0.5), radius: 2, x: 0, y: 0)
+            )
+            .padding(.horizontal, 16)
         }
         .padding(.horizontal, 16)
         .navigationTitle(viewModel.uiState.ticket.totalWithFormat)
