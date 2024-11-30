45f0206ee15f15045f9b125736349af81fecc8f5
Fernando Salom <fer@rudo.es>
2024-11-30 22:17:24 +0100

    Fix problem when receiving a notification update list

--- tickets/Presentation/App/AppDelegate.swift
+++ tickets/Presentation/App/AppDelegate.swift
@@ -11,12 +11,10 @@ class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNot
         UNUserNotificationCenter.current().delegate = self
         return true
     }
-    
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         Messaging.messaging().apnsToken = deviceToken
     }
-
 
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
         if let fcm = Messaging.messaging().fcmToken {
@@ -26,18 +24,21 @@ class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNot
     }
 
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        print(userInfo)
         NotificationCenter.default.post(name: .ticketReceived, object: nil)
     }
 
     func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                  didReceive response: UNNotificationResponse,
-                                  withCompletionHandler completionHandler: @escaping () -> Void) {
-        let userInfo = response.notification.request.content.userInfo
-
-        print(userInfo)
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
         NotificationCenter.default.post(name: .ticketReceived, object: nil)
-
         completionHandler()
-      }
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
+        NotificationCenter.default.post(name: .ticketReceived, object: nil, userInfo: userInfo)
+        completionHandler([.banner, .sound, .badge])
+    }
 }
